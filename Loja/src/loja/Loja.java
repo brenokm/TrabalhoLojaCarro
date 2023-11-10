420fc6addf77c98eff188f4027f2dc4813a0d897
brenokm <127207889+brenokm@users.noreply.github.com>
2023-11-10 04:22:46 -0300

    Add files via upload

---
+++ Loja/src/loja/Loja.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package loja;
+
+import Telas.TelaCadastroFuncionario;
+
+/**
+ *
+ * @author suporte01
+ */
+public class Loja {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TelaCadastroFuncionario t= new TelaCadastroFuncionario();
+        t.setVisible(true);
+    }
+    
+}
